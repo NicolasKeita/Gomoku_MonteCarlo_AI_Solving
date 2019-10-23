cff0cb6da7f7da5d7987928a934be3deb4aa16e7
Nicolas Keita <nicolas.keita@epitech.eu>
2019-10-23 16:58:40 +0200

    Added function _solve

--- brain.py
+++ brain.py
@@ -1,3 +1,6 @@
+X_WINS = "X_wins!"
+O_WINS = "O_wins!"
+DRAW = "Draw!"
 
 
 class Brain:
@@ -9,3 +12,29 @@ class Brain:
 
     def reset(self):
         self.one_variable = 0
+
+    def _solve(self, board):
+        static_eval = self._get_static_eval(board)
+        if static_eval == X_WINS or static_eval == O_WINS:
+            return static_eval
+        if self._is_full(board):
+            return DRAW
+        boards = self._get_all_possible_next_moves(board)
+        board_evals = [self._solve(board_tmp) for board_tmp in boards]
+        if X_WINS in board_evals:
+            return X_WINS
+        elif DRAW in board_evals:
+            return DRAW
+        else:
+            return O_WINS
+
+    def _is_full(self, board):
+        #TODO
+        return False
+
+    def _get_static_eval(self, board):
+        # TODO
+        return X_WINS
+
+    def _get_all_possible_next_moves(self, board):
+        return [board, board] # TODO
